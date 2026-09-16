e42423000a6a55bb7c397bee80020797bdab970d
Vitor Martins <54907067+VitorMartinsAlves@users.noreply.github.com>
2026-09-16 09:31:47 -0300

    Install activation rollback watchdog patch

--- src/multiseat_arch/__init__.py
+++ src/multiseat_arch/__init__.py
@@ -12,6 +12,7 @@ from .runtime_patch_v6 import install as _install_runtime_patch_v6
 from .runtime_patch_v7 import install as _install_runtime_patch_v7
 from .runtime_patch_v8 import install as _install_runtime_patch_v8
 from .runtime_patch_v9 import install as _install_runtime_patch_v9
+from .runtime_patch_v10 import install as _install_runtime_patch_v10
 
 _install_runtime_patch(_backend)
 _install_runtime_patch_v2(_backend)
@@ -22,3 +23,4 @@ _install_runtime_patch_v6(_backend)
 _install_runtime_patch_v7(_backend)
 _install_runtime_patch_v8(_backend)
 _install_runtime_patch_v9(_backend)
+_install_runtime_patch_v10(_backend)
